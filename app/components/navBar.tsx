f8cc190f00b7d8a08d3633758b21c9971d45f035
CristhoperSanchez <cris.israel.sanchez@gmail.com>
2023-09-24 22:16:07 +0000

    Initial Website Commit

---
+++ app/components/navBar.tsx
@@ -0,0 +1,13 @@
+import styles from "@/css/utils.module.css"
+
+
+
+export default function NavBar(){
+  return (
+    <>
+        <div className={styles.navMain}> 
+            <h1>Cristhoper Sanchez</h1> 
+        </div>
+    </>
+  )
+}
---
+++ app/components/projectSpace.tsx
@@ -0,0 +1,14 @@
+import styles from "@/css/utils.module.css"
+
+
+
+export default function ProjectSpace(){
+
+    return(
+        <>
+            <div className={styles.projectSpaceMain}>
+                <h2>Title</h2>
+            </div>
+        </>
+    )
+}
